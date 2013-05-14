710cd3c19d990601c9803710eecf8f934f5bff59
Hugo <filipe_nogueira16@hotmail.com>
2013-05-14 12:46:29 +0100

    Exercicio teste de recuperação

---
+++ Exercicio teste de recuperação/Teste16/Teste16/Form3.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form3
+
+    Private Sub AjudaToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles AjudaToolStripMenuItem.Click
+        Help.ShowHelp(Me, "7-zip.chm")
+    End Sub
+End Class
